96b47e368bc580eb9ea46a3571b8584e2f3f8d60
angela <adeng32@uwo.ca>
2024-01-05 01:32:44 -0500

    Updated Skills.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Wave from './components/Wave';
 import headshot from './images/headshot.jpg';
+
+import inenglishpls from './images/in-english-please-1.jpg';
+
 
 function App() {
   return (
@@ -17,18 +21,19 @@ function App() {
         <Wave/>
       </div>
       <div id = "skill-section">
-        <div className = "name">
+        <div className = "skill-name">
           <h2>Web Development</h2>
           <div className = "skills-cont">
             <Skills skill = "ReactJS"/>
-            <Skills skill = "JavaScript"/>
             <Skills skill = "HTML"/>
             <Skills skill = "CSS"/>
             <Skills skill = "NodeJS"/>
             <Skills skill = "Express"/>
+            <Skills skill = "SQL"/>
+            <Skills skill = "MongoDB"/>
           </div>
         </div>
-        <div className = "name">
+        <div className = "skill-name">
           <h2>DevOps</h2>
           <div className = "skills-cont">
             <Skills skill = "Docker"/>
@@ -36,15 +41,28 @@ function App() {
             <Skills skill = "Amazon Web Services"/>
           </div>
         </div>
-        <div className = "name">
+        <div className = "skill-name">
           <h2>Coding Profficiencies</h2>
           <div className = "skills-cont">
             <Skills skill = "Java"/>
             <Skills skill = "Python"/>
+            <Skills skill = "JavaScript"/>
           </div>
         </div>
       </div>
-      <h1>PROJECTS</h1>
+      <div id = "projects-sect">
+        <div id = "projects">
+          <h1>PROJECTS</h1>
+        </div>
+        <Projects
+          img = {inenglishpls}
+          link = "https://devpost.com/software/in-english-please"
+          title = "In English Please!"
+          desc = '"In English Please!" is a website which converts complicated ingredients and jargon into English which anyone can understand. Simply take a photo of an ingredients list, and upload it to the website. A seamless translation is then provided.'
+        />
+        <Projects/>
+        <Projects/>
+      </div>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import githubImg from '../images/github.png';
+import "./Projects.css";
 
-export default function Projects(){
-    return(
+export default function Projects(props) {
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+
+    return () => {
+        hiddenElements.forEach((el) => observer.unobserve(el));
+    };
+    }, []);
+
+    return (
         <div>
-            <div className = "project-slider">
-
-            </div>
+            <section className = "hidden" id = "project">
+                <img src = {props.img} alt = "cover-img"></img>
+                <div id = "container">
+                    <div id = "info"><h1>{props.title}</h1> <a href = {props.link}><img id = "git-img" src = {githubImg} alt = "github-icon"></img></a></div>
+                    <p>{props.desc}</p>
+                </div>
+            </section>
         </div>
-    )
+    );
 }
